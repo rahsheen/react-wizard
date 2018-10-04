2d48c9d9de6296a99a19f65627ab90dc766de157
Rahsheen Porter <raporter@htt.io>
2018-10-03 21:57:26 -0400

    feat: Update tests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React, {Component} from 'react'
+import Wizard from './Wizard'
 
 export default class extends Component {
   render() {
-    return <div>
-      <h2>Welcome to React Deez Nuts</h2>
-    </div>
+    return <Wizard>
+    <Wizard.Step>
+      {({ nextStep, prevStep }) => {
+        return (
+          <div>
+            <h2>Foo</h2>
+            <button onClick={prevStep}>Back</button>
+            <button onClick={nextStep}>Next</button>
+          </div>
+        );
+      }}
+    </Wizard.Step>
+    </Wizard>
   }
 }
