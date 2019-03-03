8902d2d935d2a0827381583030485f3696865669
Rahsheen Porter <rahsheen.porter@gmail.com>
2019-03-03 10:48:45 -0500

    Update test imports and some code formatting

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,33 +1,39 @@
-import expect from 'expect'
-import React from 'react'
-import {render, unmountComponentAtNode} from 'react-dom'
+import expect from "expect"
+import React from "react"
+import { render, unmountComponentAtNode } from "react-dom"
 
-import Wizard from 'src/'
+import { Wizard } from "src/"
 
-describe('Wizard', () => {
+describe("Wizard", () => {
   let node
 
   beforeEach(() => {
-    node = document.createElement('div')
+    node = document.createElement("div")
   })
 
   afterEach(() => {
     unmountComponentAtNode(node)
   })
 
-  it('displays a welcome message', () => {
-    render(<Wizard><Wizard.Step>{() => <h2>>@rahsheen/React-Wizard</h2>}</Wizard.Step></Wizard>, node, () => {
-      expect(node.innerHTML).toContain('React-Wizard')
-    })
-  }) 
+  it("displays a welcome message", () => {
+    render(
+      <Wizard>
+        <Wizard.Step>{() => <h2>>@rahsheen/React-Wizard</h2>}</Wizard.Step>
+      </Wizard>,
+      node,
+      () => {
+        expect(node.innerHTML).toContain("React-Wizard")
+      }
+    )
+  })
 
-  it('does not render without children', () => {
-    render(<Wizard></Wizard>, node, () => {
-      expect(node.children.length).toBe(0);
+  it("does not render without children", () => {
+    render(<Wizard />, node, () => {
+      expect(node.children.length).toBe(0)
     })
   })
 
-  it('does not render future steps', () => {
+  it("does not render future steps", () => {
     const structure = (
       <Wizard>
         <Wizard.Step>{() => <h2>>@rahsheen/React-Wizard</h2>}</Wizard.Step>
@@ -35,7 +41,7 @@ describe('Wizard', () => {
       </Wizard>
     )
     render(structure, node, () => {
-      expect(node.innerHTML).toNotContain('Step 2');
+      expect(node.innerHTML).toNotContain("Step 2")
     })
   })
 })
